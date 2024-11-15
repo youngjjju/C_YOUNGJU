39a2a7920fdad176232d0f7553e842405b09910c
KIM YOUNGJU <2000dudwn@naver.com>
2024-11-15 21:26:12 +0900

    Create 버퍼비우기_개행문자제거.c

---
+++ Others/버퍼비우기_개행문자제거.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int CleanInputBuffer() {
+	while (getchar() != '\n') {};
+}
+
+int main() {
+	char perID[7];
+	char name[10];
+
+	fputs("주민번호 앞 6자리 입력: ", stdout);
+	fgets(perID, sizeof(perID), stdin);
+	CleanInputBuffer();
+		
+	fputs("이름 입력: ", stdout);
+	fgets(name, sizeof(name), stdin);
+
+	printf("주민번호: %s \n", perID);
+	printf("이름: %s \n", name);
+}
