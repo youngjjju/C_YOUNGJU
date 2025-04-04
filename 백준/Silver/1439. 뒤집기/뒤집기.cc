f1f39a41acde42c7476f4b9c147ebbc10a7e2c9f
KIM YOUNGJU <2000dudwn@naver.com>
2025-04-05 05:33:34 +0900

    [Silver V] Title: 뒤집기, Time: 0 ms, Memory: 2876 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.cc
+++ 백준/Silver/1439. 뒤집기/뒤집기.cc
@@ -1,21 +1,23 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main() {
-    static char nums[1000001];  // 1MB 이상은 static 또는 전역 변수 추천
-    int zero = 0, one = 0;
-    cin >> nums;
 
-    // 첫 문자 처리
-    if (nums[0] == '0') zero++;
-    else one++;
-
-    // 직접 strlen 쓰지 말고 while로 길이 탐색
-    for (int i = 1; nums[i] != '\0'; i++) {
-        if (nums[i] == nums[i - 1]) continue;
-        if (nums[i] == '0') zero++;
-        else one++;
-    }
-
-    cout << (zero <= one ? zero : one);
+	char nums[1000000];
+	int zero = 0;
+	int one = 0;
+	cin >> nums;
+	if (nums[0] == '0') { zero++; }
+	else { one++; }
+	for (int i = 1; i < strlen(nums); i++) {
+		
+		if (nums[i] == nums[i - 1]) {
+			continue;
+		}
+		if (nums[i] == '0') { zero++; }
+		else { one++; }
+	}
+	zero = (zero <= one) ? zero : one;
+	cout << int(zero);
 }
