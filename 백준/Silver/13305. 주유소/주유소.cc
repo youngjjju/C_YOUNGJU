bc175f9b1f962364a1cf06d75b712b71c1d8f9d8
KIM YOUNGJU <2000dudwn@naver.com>
2025-04-12 05:31:50 +0900

    [Silver III] Title: 주유소, Time: 84 ms, Memory: 3588 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -1,46 +1,28 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <numeric>
 using namespace std;
 
 int main() {
-	int N;
-	cin >> N;
-	vector<int> distance;
-	vector<int> price;
-	int moneyPoint[1000];
-	for (int i = 0; i < N-1; i++) {
-		int temp;
-		cin >> temp;
-		distance.push_back(temp);
-	}
-	for (int i = 0; i < N; i++) { 
-		int temp;
-		cin >> temp;
-		price.push_back(temp);
-	}
-	int cheapest = min_element(price.begin(), price.end())-price.begin();
-	moneyPoint[0]=cheapest;
-	int moneyPointIndex = 1;
-	while(cheapest!=0){
-		vector<int> newPrice(price.begin(), price.begin() + cheapest);
-		int prevCheapest = cheapest;
-		cheapest = min_element(newPrice.begin(), newPrice.end()) - newPrice.begin();
-		if (prevCheapest < cheapest) {break;}
-		moneyPoint[moneyPointIndex]=cheapest;
-		moneyPointIndex++;
-	}
-	sort(moneyPoint, moneyPoint + moneyPointIndex);
-	int totalMoney = 0;
-	int i=0;
-	while (i+1 < moneyPointIndex) {
-		totalMoney += price[moneyPoint[i]] * (moneyPoint[i+1] - moneyPoint[i]);
-		i++;
-	}
-	if (moneyPointIndex == 1) {
-		cout << accumulate(distance.begin(), distance.end(), 0)*price[0];
-	}else {
-		cout << totalMoney;
-	}
+    int N;
+    cin >> N;
+
+    vector<long long> distance(N - 1);
+    vector<long long> price(N);
+
+    for (int i = 0; i < N - 1; i++) {
+        cin >> distance[i];
+    }
+    for (int i = 0; i < N; i++) {
+        cin >> price[i];
+    }
+
+    long long total = 0;
+    long long minPrice = price[0];
+
+    for (int i = 0; i < N - 1; i++) {
+        if (price[i] < minPrice) minPrice = price[i];
+        total += minPrice * distance[i];
+    }
+
+    cout << total << endl;
 }
