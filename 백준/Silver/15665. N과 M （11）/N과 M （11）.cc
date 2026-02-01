0dfeb9a02b5de09b5d11601ee508abc7adbb8c0c
KIM YOUNGJU <2000dudwn@naver.com>
2026-02-01 23:47:38 +0900

    [Silver II] Title: N과 M (11), Time: 88 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,35 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int N, M, last;
+int arr_in[10], arr_out[10];
+
+void func(int k) {
+	if (k == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr_out[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	int last = 0;
+	for (int i = 0; i < N; i++) {
+		if (arr_in[i] == last) continue;		
+		arr_out[k] = arr_in[i];
+		last = arr_out[k];
+		func(k + 1);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		int tmp; cin >> tmp;
+		arr_in[i] = tmp;
+	}
+	sort(arr_in, arr_in + N);
+	func(0);
+}
