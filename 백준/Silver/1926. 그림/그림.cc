0aa63f0720b8b75230c7faaf6b5eb38fbc531a75
KIM YOUNGJU <2000dudwn@naver.com>
2026-03-03 23:06:43 +0900

    [Silver I] Title: 그림, Time: 20 ms, Memory: 5040 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,55 +1,65 @@
 #include "bits/stdc++.h"
+
 using namespace std;
 
+int n, m;
+int arr[500][500];
+int visited[500][500];
+
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, 1, -1 };
+
+int MAX;
+int NUM;
+
+stack<pair<int, int>> v;
+
+int isOverArr(int row, int col) {
+	if (row < 0 || row >= n || col < 0 || col >= m) return 1;
+	return 0;
+}
+
 int main() {
-	ios::sync_with_stdio(0);
 	cin.tie(0);
+	ios::sync_with_stdio(0);
 
-	int row, col, num_pic(0), result(0);
-	int back[500][500] = {0,};
-	int visit[500][500] = {0,};
-	int dx[4] = { 1, -1, 0, 0 };
-	int dy[4] = { 0, 0, -1, 1 };
+	cin >> n >> m;
 
-	cin >> row >> col;
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			int tmp;
-			cin >> tmp;
-			back[i][j] = tmp;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j];
 		}
 	}
-	
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			int tmp_MAX = 0;
+			if (arr[i][j] == 1 && !visited[i][j]) {
+				v.push(make_pair(i, j));
+				visited[i][j] = 1;
+				tmp_MAX++;
+				NUM++;
+			}
 			
-			if (!back[i][j] || visit[i][j]) continue;
-			deque <pair<int, int>> Q;
-			Q.push_back({ i,j });
-			visit[i][j] = 1;
-			num_pic++;
-			int mx(0);
-
-			while (!Q.empty()) {
-				int tmp_x = Q.front().first;
-				int tmp_y = Q.front().second;
-				Q.pop_front();
-				mx++;
-				for (int k=0; k < 4; k++) {
-					int new_x = tmp_x + dx[k];
-					int new_y = tmp_y + dy[k];
-
-					if (new_x < 0 || new_x >= row || new_y < 0 || new_y >= col) continue;
-					if (visit[new_x][new_y] || !back[new_x][new_y]) continue;
-
-					visit[new_x][new_y] = 1;
-					Q.push_back({ new_x, new_y });
+			while (!v.empty()) {
+				int row = v.top().first;
+				int col = v.top().second;
+				v.pop();
+				for (int k = 0; k < 4; k++) {
+					int nx = row + dx[k];
+					int ny = col + dy[k];
+					if (!isOverArr(nx, ny) && arr[nx][ny] == 1 && visited[nx][ny] != 1) {
+						v.push({ nx, ny });
+						visited[nx][ny] = 1;
+						tmp_MAX++;
+					}
 				}
 			}
-			result = max(mx, result);
+			if (tmp_MAX > MAX) MAX = tmp_MAX;
 		}
 	}
-	cout << num_pic << '\n';
-	cout << result;
+
+	cout << NUM << '\n' << MAX;
 	return 0;
+
 }
